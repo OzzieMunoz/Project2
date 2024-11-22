1c7c17e05db208cc3a042fc2712e8dc7294bc7cd
OzzieMunoz <osvaldomunoz1205@gmail.com>
2024-11-22 12:45:41 -0800

    Added login functionality

--- app/src/main/java/com/example/bullshit/LoginActivity.java
+++ app/src/main/java/com/example/bullshit/LoginActivity.java
@@ -1,6 +1,12 @@
 package com.example.bullshit;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
@@ -9,6 +15,23 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        // Add login functionality later
+
+        EditText usernameInput = findViewById(R.id.usernameInput);
+        EditText passwordInput = findViewById(R.id.passwordInput);
+        Button loginButton = findViewById(R.id.loginSubmitButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = usernameInput.getText().toString();
+                String password = passwordInput.getText().toString();
+
+                if (username.equals("testuser1") && password.equals("testuser1")) {
+                    Toast.makeText(LoginActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(LoginActivity.this, "Invalid credentials", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
